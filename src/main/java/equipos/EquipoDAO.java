68a1fa61672ceebfff5e859b898a0a154ddb9a2b
Roovers <91910203+Roovers@users.noreply.github.com>
2023-12-05 11:57:00 -0300

    Update EquipoDAO.java

--- src/main/java/equipos/EquipoDAO.java
+++ src/main/java/equipos/EquipoDAO.java
@@ -3,23 +3,19 @@ package equipos;
 import java.util.List;
 
 public interface EquipoDAO {
-    // Crea un equipo en la base de datos
+    
     void crearEquipoBocaJuniors();
 
     void crearEquipoRiverPlate();
 
     void crearEquipo();
-
-    // Obtiene un equipo por su nombre
+    
     Equipo obtenerEquipo();
 
-    // Actualiza la información de un equipo en la base de datos
     void actualizarEquipo();
 
-    // Elimina un equipo por su nombre
     void eliminarEquipo();
 
-    // Obtiene una lista de todos los equipos en la base de datos
     List<Equipo> obtenerTodosLosEquipos();
 
 }
